fc3c8b8b53b68e2386621e02659fbceb49d3d248
Miln Blaz <milan@vivant.eco>
2021-05-30 21:09:14 +0200

    init

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TestView from './src/views/TestView';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TestView text='Boo ya madafaka' />
       <StatusBar style="auto" />
     </View>
   );
